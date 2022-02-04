ca5d1a2453b361a4409aeddf75d16e535018900a
ravimengal <mengal22ravi@gmail.com>
2022-02-04 19:07:40 +0530

    Cleaning Folder Str

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Second />
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+import Second, {} from "./components/Second.vue"
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,
+    Second
+  
   }
 }
 </script>
---
+++ src/components/Second.vue
@@ -0,0 +1,16 @@
+<template>
+    <h1>Hello from second component</h1>
+</template>
+
+<script>
+export default {
+    name:"Second"
+
+}
+</script>
+
+<style>
+h1{
+    color:red;
+}
+</style>
